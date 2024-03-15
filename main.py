e68e3971f7099bb153239cb784c617af70919f0b
Victor-Andrei NONEA (94403) <victor_andrei.nonea@stud.acs.upb.ro>
2024-03-15 15:45:18 +0100

    rewrote main to include pytorch model

--- main.py
+++ main.py
@@ -1,34 +1,63 @@
 import gymnasium as gym
 import random as rn
-env = gym.make("CartPole-v1", render_mode='human')
+import torch
 
-observation, info = env.reset(seed=42)
-for _ in range(1000):
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
+from model import model
 
-    if terminated or truncated:
-        observation, info = env.reset()
-env.close()
+# env = gym.make("CartPole-v1", render_mode='human')
 
-def train_dqn(model, N, M, sample_size=100, gamma=0.9):
+# observation, info = env.reset(seed=42)
+# for _ in range(1000):
+#    action = env.action_space.sample()
+#    observation, reward, terminated, truncated, info = env.step(action)
+
+#    if terminated or truncated:
+#        observation, info = env.reset()
+#env.close()
+
+def train_dqn(model, N, M, sample_size=100, gamma=0.9, learning_rate=1e-3, render_mode=None):
+    env = gym.make("CartPole-v1", render_mode=render_mode)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    loss_fn = torch.nn.MSELoss()
+    
+    to_torch = lambda ss: torch.tensor(ss, dtype=torch.float32)
+
     D = []
     for ep in range(1, M + 1):
         s, _ = env.reset()
         for t in range(1, 501):
-            q_values = model(s)
-            a = selection_policy(q_values)
+            q_values = model(to_torch([s]))[0]
+            a = q_values.argmax().item()
             s_next, r, term, trunc, _ = env.step(a)
             if len(D) < N:
                 D.append((s, a, r, s_next, term or trunc))
             else:
                 D[rn.randrange(len(D))] = (s, a, r, s_next, term or trunc)
             
-            if len(D) > sample_size
+            if len(D) > sample_size:
                 samples = rn.sample(D, sample_size)
             else:
                 samples = D
             
-            ys = [r if term else r + gamma * max(model(s)) for s, a, r, s_next, term in samples]
+            s = s_next
             
-            model.optimize([(y - model(s)[a]) ** 2 for (s, a, _, _, _), y in zip(samples, ys))])
+            ys = [r if term else r + gamma * max(model(to_torch([s_next]))[0]) for s, a, r, s_next, term in samples]
+            ss, _as, rs, _, terms = zip(*samples)
+            ss = [to_torch(s) for s in ss]
+            
+            # model.optimize([(y - model(s)[a]) ** 2 for (s, a, _, _, _), y in zip(samples, ys))])
+            
+            
+            # print(torch.stack(ss).shape)
+            loss = loss_fn(model(torch.stack(ss))[:, _as], to_torch(ys))
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            
+            if term or trunc:
+                break
+    env.close()
+
+train_dqn(model, 100, 100)
