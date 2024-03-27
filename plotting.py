1efc1f0d741e60d0519ff0288ad9d0c7fab06db2
Sybe <sybevanbenthum@hotmail.com>
2024-03-27 14:08:46 +0100

    Added plot file

---
+++ plotting.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+import json
+
+from Helper import smooth
+from Helper import LearningCurvePlot
+
+
+# func to plot
+
+# code to plot experiments
+def plot_experiments():
+    pass
+
+if __name__ == '__main__':
+    plot_experiments()
