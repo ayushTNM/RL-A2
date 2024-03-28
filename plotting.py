4bd04feb9510aad22e59f80e25ef5e10f8fc04e7
Sybe <sybevanbenthum@hotmail.com>
2024-03-28 13:50:15 +0100

    Made plotting work

--- plotting.py
+++ plotting.py
@@ -7,47 +7,52 @@ from Helper import LearningCurvePlot
 
 
 # func to plot
-def plot(data, names, title, param_name):
+
+#TODO add option for boolean values
+def plot_hyperparameter(data, names, title, param_name, param_key, filename):
     plot = LearningCurvePlot(title)
     for name in names:
         experiment = data[name]
         result = np.array(experiment['results']).T
         x = result[0]
         y = result[1]
-        param = experiment[param_name]
-        label = param_name + ": " + param
+        param = experiment[param_key]
+        label = param_name + ": " + str(param)
         plot.add_curve(x, y, label)
+    plot.save(filename)
+
+def plot_compare_tn_rb(data, names, title, filename):
+    plot = LearningCurvePlot(title)
+    for name in names:
+        experiment = data[name]
+        result = np.array(experiment['results']).T
+        x = result[0]
+        y = result[1]
+        label = "DQN"
+        if "tn" in name:
+            label += " with Target Network on"
+        if "rb" in name:
+            label += " with Replay Buffer on"
+        plot.add_curve(x, y, label)
+    plot.save(filename)
 
 
 # code to plot experiments
 def plot_experiments():
-    data_path = "data.json"
-    if os.path.exists(data_path):
-        # Load the JSON file
-        with open(data_path, 'r') as file:
-            data = json.load(file)
-        print("JSON file loaded successfully.")
-    else:
-        data = {}
-        print("JSON file does not exist.")
-    print(data['DQN'])
-    k = data.keys()
-    print(k)
+    data = load_data("data.json")
     experiments = extract(data)
-
+    
     # plot 1 Learning rate
     names = ['DQN', 'DQN_rb_1000']
-    title, param_name = "Learning rate compared of DQN agents", "Learning rate"
-    plot(data, names, title, param_name)
+    title, param_key, param_name = "Learning rate compared of DQN agents", "learning_rate", "Learning rate"
+    plot_hyperparameter(data, names, title, param_name, param_key, "lr.png")
 
     # plot 2
-    names = ['DQN', 'DQN_rb_1000']
-    title, param_name = " ε compared of DQN agents", "ε"
-    plot(data, names, title, param_name)
-
+    names = ['DQN', 'DQN_rb_1000', 'DQN_tn_100', 'DQN_rb_1000_tn_100']
+    title = "Target Network and Replay Buffer compared"
+    plot_compare_tn_rb(data, names, title, "TN RB compared.png")
 
     print()
-    #TODO settings to show which hyper parameter
 
 def extract(data):
     experiments = []
@@ -56,6 +61,19 @@ def extract(data):
         experiment['name'] = key
         experiments.append(experiment)
     return experiments
+
+def load_data(data_path):
+    if os.path.exists(data_path):
+        # Load the JSON file
+        with open(data_path, 'r') as file:
+            data = json.load(file)
+        print("JSON file loaded successfully.")
+    else:
+        data = {}
+        print("JSON file does not exist.")
+    k = data.keys()
+    print(k)
+    return data
 
 if __name__ == '__main__':
     plot_experiments()
